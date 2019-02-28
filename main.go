0f1b120eaead3591d71084915d676451eeedf2f1
Gabriel D'Intino <gdintino@gmail.com>
2019-02-27 23:19:59 -0300

    Creo que anda

--- main.go
+++ main.go
@@ -11,8 +11,8 @@ import (
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
-	http.HandleFunc("/spliter/v1/ping", v1.PingV1)
-	http.HandleFunc("/spliter/v1/ingest", v1.IngestFileV1)
+	http.HandleFunc("/splitter/v1/ping", v1.PingV1)
+	http.HandleFunc("/splitter/v1/ingest", v1.IngestFileV1)
 	fmt.Println(fmt.Sprintf("Server started at: %s", configurations.Port))
 	http.ListenAndServe(fmt.Sprintf(":%s", configurations.Port), nil)
 }
